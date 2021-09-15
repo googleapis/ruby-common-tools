07660cd6c74fa07b1fda70a54956c1ae06b94a85
Daniel Azuma <dazuma@google.com>
2021-09-15 19:27:07 +0000

    feat: Owlbot preprocessor for Ruby

---
+++ owlbot-postprocessor/README.md
@@ -0,0 +1,142 @@
+# OwlBot postprocessor for Ruby
+
+This is the OwlBot postprocessor for Ruby. It is designed to work with
+[OwlBot](https://github.com/googleapis/repo-automation-bots/tree/main/packages/owl-bot)
+to regenerate [Ruby GAPIC clients](https://github.com/googleapis/google-cloud-ruby)
+by providing the logic to merge newly generated client code with an existing
+Ruby library.
+
+## Usage
+
+This postprocessor is invoked directly by OwlBot. A library requests use of
+this postprocessor by referencing it in the `.OwlBot.yaml` file. It can also
+optionally customize the postprocessor by providing a Ruby configuration file.
+
+### Basic usage
+
+To configure a Ruby GAPIC client for OwlBot, create a `.OwlBot.yaml` file in
+the library directory. It _must_ instruct OwlBot to copy the newly generated
+library code into the directory `/owl-bot-staging/$GEM_NAME` in the repo. It
+also _should_ specify the Ruby postprocessor Docker image
+`gcr.io.cloud-devrel-public-resources/owlbot-ruby:latest` (unless a global
+`/.github/.OwlBot.yaml` file already does so).
+
+Here is an example OwlBot config:
+
+```yaml
+# /google-cloud-access_approval-v1/.OwlBot.yaml
+
+docker:
+  image: gcr.io.cloud-devrel-public-resources/owlbot-ruby:latest
+deep-copy-regex:
+  - source: /google/cloud/accessapproval/v1/google-cloud-accessapproval-v1-ruby/(.*)
+    dest: /owl-bot-staging/google-cloud-access_approval-v1/$1
+```
+
+This postprocessor will then, by default:
+
+* Copy generated files from the staging directory into the gem directory.
+* Preserve any existing `version.rb` (assuming it is in the expected location)
+  and `CHANGELOG.md` files.
+* Preserve existing copyright years in Ruby source files.
+* Delete any files that were _previously generated_ but are no longer
+  generated. (Does not touch files that were added by hand.)
+* Create a file called `.owlbot-manifest.json` in the library directory that
+  tracks the list of generated files for the previous step.
+
+Note: This preprocessor is intended to be used with a "staging" strategy for
+OwlBot. That is, OwlBot itself copies newly generated files into a staging
+directory, and the postprocessor is responsible for moving the files from there
+into the final location. As a result, the `deep-remove-regex` and
+`deep-preserve-regex` OwlBot configuration keys are probably not useful.
+
+### Customizing the behavior
+
+A library can customize the behavior of this postprocessor by providing a file
+in the library directory called `.owlbot.rb`. If present, this file is executed
+by the postprocessor and _must_ perform all the needed functionality, including
+copying files from the staging directory, and making any desired changes.
+
+For this purpose, the `.owlbot.rb` file will have access to the `OwlBot` module
+which provides useful methods for implementing postprocessing functionality.
+In general, usage of this module involves first configuring various filters and
+content manipulation routines, and then finally calling `OwlBot.move_files`
+which performs the file copying subject to that configuration.
+
+Following is an example `.owlbot.rb` file:
+
+```ruby
+# .owlbot.rb
+
+# Install a content modifier that renames a particular class in all Ruby files
+OwlBot.modifier path: %r{^lib/.*\.rb$} do |content|
+  content.gsub "BadlyNamedClass", "BetterName"
+end
+
+# Omit a particular generated file by removing it from the staging directory
+FileUtils.rm_f File.join(OwlBot.staging_dir, "unnecessary.txt")
+
+# Perform the file moves
+OwlBot.move_files
+```
+
+By default, the `OwlBot` module is preconfigured with the default postprocessor
+behavior: preservation of `version.rb` and `CHANGELOG.md` and preservation of
+copyright years. Thus, the following `.owlbot.rb` file would have an identical
+effect to not having an `.owlbot.rb` file at all:
+
+```ruby
+# "default" .owlbot.rb
+
+# Just perform the file moves with the default configuration
+OwlBot.move_files
+```
+
+As a result, if you want to override/disable the default behavior, you will
+need to remove the existing configurations. For example:
+
+```ruby
+# .owlbot.rb
+
+# The changelog and version files are named differently in this library,
+# so remove the default preserved paths and add new ones.
+OwlBot.preserved_paths.clear
+OwlBot.preserve path: ["HISTORY.md",
+                       "lib/#{OwlBot.gem_name.tr '-', '/'}/gem_version.rb"]
+
+OwlBot.move_files
+```
+
+For detailed reference documentation on the `OwlBot` module, see the file
+`lib/owlbot.rb`.
+
+## Development
+
+The Ruby preprocessor lives in the GitHub repository
+https://github.com/googleapis/ruby-common-tools in the directory
+`/owlbot-preprocessor`. Its implementation code is in the `lib` subdirectory,
+and tests are provided in the `test` subdirectory.
+
+### Development tools
+
+The `toys` gem is used to run tests and other tasks. Install it using
+`gem install toys`.
+
+Under the `/owlbot-preprocessor` directory, the following tools are available:
+
+* `toys build` : Builds a local image of the preprocessor.
+* `toys test` : Runs the unit tests. Requires a local preprocessor image.
+* `toys rubocop` : Runs rubocop lint and style checks.
+* `toys ci` : Runs both test and rubocop.
+
+Some commands have flags that can be passed in. Pass the `--help` flag to any
+command for more details.
+
+### Releases
+
+Releases are handled via release-please. After a release is tagged on GitHub,
+a Cloud Build job will trigger that builds the production image (with the
+`latest` Docker image tag.)
+
+It is also possible to build "ad-hoc" test images using the `toys release dev`
+command. These images are tagged with a timestamp rather than `latest`.
--- toys/release/please.rb
+++ toys/release/please.rb
@@ -20,7 +20,9 @@ flag :install
 flag :use_fork, "--fork"
 flag :base_dir, "--base-dir=PATH"
 flag :release_type, "--release-type=TYPE", default: "ruby"
+flag :non_gem, "--non-gem"
 flag :version_file, "--version-file=PATH"
+flag :version_expr, "--version-expr=EXPR"
 flag :repo_url, "--repo-url=NAME"
 flag :retries, "--retries=TIMES", default: 1, accept: Integer
 flag :delay, "--delay=SECS", default: 2, accept: Numeric
@@ -28,7 +30,9 @@ flag :retry_delay, "--retry-delay=SECS", default: 4, accept: Numeric
 flag :github_event_name, "--github-event-name=NAME"
 flag :github_token, "--github-token=TOKEN"
 
-remaining_args :input_gems, desc: "Release the specified gems. If no specific gem is provided, all gems are checked."
+remaining_args :input_packages do
+  desc "Release the specified packages. If no specific packages are provided, all are checked."
+end
 
 include :exec, e: true
 include :terminal, styled: true
@@ -37,12 +41,10 @@ def run
   check_github_context
   Dir.chdir context_directory
   handle_install
-  set :repo_url, default_repo_url unless repo_url
-  set :github_token, default_github_token unless github_token
-  gem_info = input_gems.empty? ? find_all_gems : interpret_input_gems
+  package_info = init_info
 
   @errors = []
-  gem_info.each do |name, version, dir|
+  package_info.each do |name, version, dir|
     release_please name, version, dir
   end
 
@@ -66,36 +68,65 @@ def handle_install
   exit 0
 end
 
-def find_all_gems
-  prefix = base_dir ? "#{base_dir}/" : ""
-  (Dir.glob("#{prefix}*.gemspec") + Dir.glob("#{prefix}*/*.gemspec")).map do |path|
-    [File.basename(path, ".gemspec"), nil, File.dirname(path)]
-  end
+def init_info
+  set :repo_url, default_repo_url unless repo_url
+  set :github_token, default_github_token unless github_token
+  package_info = input_packages.empty? ? find_all_packages : interpret_input_packages
+  raise "Cannot provide --version-file with multiple packages" if package_info.size > 1 && version_file
+  raise "Cannot provide --version-expr with multiple packages" if package_info.size > 1 && version_expr
+  package_info
 end
 
-def interpret_input_gems
+def find_all_packages
   prefix = base_dir ? "#{base_dir}/" : ""
-  input_gems.map do |input_gem|
-    name, version = input_gem.split ":"
-    paths = Dir.glob("#{prefix}#{name}.gemspec") + Dir.glob("#{prefix}*/#{name}.gemspec")
-    if paths.empty?
-      logger.error "Unable to find gem #{name} in the repo"
-      exit 1
-    elsif paths.size > 1
-      logger.error "Found multiple gemspecs for gem #{name} in the repo"
-      exit 1
+  if non_gem
+    Dir.glob("#{prefix}*/CHANGELOG.md").map do |path|
+      dir = File.dirname path
+      [File.basename(dir), nil, dir]
     end
-    [name, version, File.dirname(paths.first)]
+  else
+    (Dir.glob("#{prefix}*.gemspec") + Dir.glob("#{prefix}*/*.gemspec")).map do |path|
+      [File.basename(path, ".gemspec"), nil, File.dirname(path)]
+    end
   end
 end
 
-def release_please gem_name, release_as, dir
-  cur_version = gem_version gem_name, dir
-  job_name = "release-please for #{gem_name} from version #{cur_version}"
+def interpret_input_packages
+  prefix = base_dir ? "#{base_dir}/" : ""
+  input_packages.map do |input|
+    name, version = input.split ":"
+    version = nil if version == ""
+    path =
+      if non_gem
+        unless File.file? "#{prefix}#{name}/CHANGELOG.md"
+          logger.error "Unable to find package directory #{name} in the repo"
+          exit 1
+        end
+        "#{prefix}#{name}"
+      else
+        paths = Dir.glob("#{prefix}#{name}.gemspec") + Dir.glob("#{prefix}*/#{name}.gemspec")
+        if paths.empty?
+          logger.error "Unable to find gem #{name} in the repo"
+          exit 1
+        elsif paths.size > 1
+          logger.error "Found multiple gemspecs for gem #{name} in the repo"
+          exit 1
+        else
+          paths.first
+        end
+      end
+    [name, version, path]
+  end
+end
+
+def release_please package_name, release_as, dir
+  version_path = version_file || default_version_path(dir, package_name)
+  cur_version = package_version package_name, dir, version_path
+  job_name = "release-please for #{package_name} from version #{cur_version}"
   job_name = "#{job_name} as version #{release_as}" if release_as
   logger.info "Running #{job_name}"
 
-  cmd = build_command dir, gem_name, cur_version, release_as
+  cmd = build_command dir, package_name, cur_version, release_as, version_path
   log_cmd = cmd.inspect
   log_cmd.sub! github_token, "****" if github_token
 
@@ -117,21 +148,34 @@ def release_please gem_name, release_as, dir
   sleep delay
 end
 
-def gem_version gem_name, dir
-  func = proc do
-    Dir.chdir dir do
-      spec = Gem::Specification.load "#{gem_name}.gemspec"
-      puts spec.version.to_s
+def package_version package_name, dir, version_path
+  func =
+    if non_gem
+      raise "Need --version-path for a non-gem package release" unless version_path
+      expr = version_expr || default_version_expr(package_name)
+      constants = expr.split "::"
+      proc do
+        Dir.chdir dir do
+          load version_path
+          puts constants.reduce(::Object) { |cur, name| cur.const_get name.to_sym }
+        end
+      end
+    else
+      proc do
+        Dir.chdir dir do
+          spec = Gem::Specification.load "#{package_name}.gemspec"
+          puts spec.version.to_s
+        end
+      end
     end
-  end
   version = capture_proc(func).strip
   version == "0.0.1alpha" ? nil : version
 end
 
-def build_command dir, gem_name, cur_version, release_as
+def build_command dir, package_name, cur_version, release_as, version_path
   cmd = [
     "npx", "release-please", "release-pr",
-    "--package-name", gem_name,
+    "--package-name", package_name,
     "--path", dir,
     "--release-type", release_type,
     "--repo-url", repo_url,
@@ -143,7 +187,6 @@ def build_command dir, gem_name, cur_version, release_as
   cmd += ["--last-package-version", cur_version] if cur_version && cur_version >= "0.1"
   cmd += ["--release-as", release_as] if release_as
   cmd += ["--token", github_token] if github_token
-  version_path = version_file || default_version_path(dir, gem_name)
   cmd += ["--version-file", version_path] if version_path
   cmd
 end
@@ -164,10 +207,10 @@ def default_github_token
   match ? match[1] : nil
 end
 
-def default_version_path dir, gem_name
+def default_version_path dir, package_name
   path = nil
   Dir.chdir dir do
-    path = File.join "lib", gem_name.gsub("-", "/"), "version.rb"
+    path = File.join "lib", package_name.gsub("-", "/"), "version.rb"
     path = nil unless File.file? path
     if path.nil?
       paths = Dir.glob "lib/**/version.rb"
@@ -180,4 +223,12 @@ def default_version_path dir, gem_name
     logger.warn "Unable to find version.rb!"
   end
   path
+end
+
+def default_version_expr package_name
+  namespace = package_name
+              .split("-")
+              .map { |segment| segment.split("_").map(&:capitalize).join }
+              .join "::"
+  "#{namespace}::VERSION"
 end
