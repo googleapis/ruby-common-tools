199c3d93eb336c2602f80d6797b63279eb05a9c7
Daniel Azuma <dazuma@google.com>
2021-11-17 00:10:05 +0000

    fix(rad): Render non-markdown files reasonably as preformatted text

--- toys/release/.data/rad-yard-templates/default/fulldoc/yaml/setup.rb
+++ toys/release/.data/rad-yard-templates/default/fulldoc/yaml/setup.rb
@@ -26,27 +26,52 @@ def copy_files
   # copy markdown files into the yard output folder
   options.files.each do |file|
     dest_filename = file.filename == readme_filename ? "index.md" : file.filename
-    File.open "#{options.serializer.basepath}/#{dest_filename}", "w" do |dest|
-      in_code_state = 0
-      File.readlines(file.filename).each do |line|
-        case in_code_state
-        when 0
-          if line.strip == "```ruby"
-            in_code_state = 1
-            next
-          end
-        when 1
-          line = "<pre class=\"prettyprint lang-rb\">#{line}"
-          in_code_state = 2
-        when 2
-          if line.strip == "```"
-            in_code_state = 0
-            line = "</pre>\n"
-          end
-        end
-        dest.puts line
-      end
+    case File.extname dest_filename
+    when ".md"
+      copy_markdown_file file.filename, dest_filename
+    when ".txt", ""
+      copy_text_file file.filename, dest_filename
     end
+  end
+end
+
+def copy_markdown_file source_filename, dest_filename
+  source_lines = File.readlines source_filename
+  dest_path = File.join options.serializer.basepath, dest_filename
+  File.open dest_path, "w" do |dest|
+    in_code_state = 0
+    source_lines.each do |line|
+      case in_code_state
+      when 0
+        if line.strip == "```ruby"
+          in_code_state = 1
+          next
+        end
+      when 1
+        line = "<pre class=\"prettyprint lang-rb\">#{line}"
+        in_code_state = 2
+      when 2
+        if line.strip == "```"
+          in_code_state = 0
+          line = "</pre>\n"
+        end
+      end
+      dest.puts line
+    end
+  end
+end
+
+def copy_text_file source_filename, dest_filename
+  source_content = File.read source_filename
+  dest_content = source_content.gsub(/(^|\n+)([^\n])/, "\\1    \\2")
+  dest_content = "# #{source_filename}\n\n#{dest_content}"
+
+  base_name = File.basename dest_filename, ".*"
+  dest_filename = "#{base_name}.md"
+  dest_path = File.join options.serializer.basepath, dest_filename
+
+  File.open dest_path, "w" do |dest|
+    dest.puts dest_content
   end
 end
 
