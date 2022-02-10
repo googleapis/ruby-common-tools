d5595d90167202a5f848ee02bd459e1c5a1b8450
Daniel Azuma <dazuma@google.com>
2022-02-10 23:15:55 +0000

    feat(toys): Move yoshi tools here

---
+++ toys/yoshi/.toys.rb
@@ -0,0 +1,35 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+mixin "yoshi-utils" do
+  def yoshi_utils
+    @yoshi_utils ||= begin
+      require_relative ".lib/yoshi/utils"
+      ::Yoshi::Utils.new context: self
+    end
+  end
+end
+
+mixin "yoshi-pr-generator" do
+  on_include do
+    include "yoshi-utils" unless include? "yoshi-utils"
+  end
+
+  def yoshi_pr_generator
+    @yoshi_pr_generator ||= begin
+      require_relative ".lib/yoshi/pr_generator"
+      ::Yoshi::PrGenerator.new yoshi_utils: yoshi_utils
+    end
+  end
+end
