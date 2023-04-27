768b07e3db455a0f5f592c16cdd63ea1a6ca76bf
Daniel Azuma <dazuma@google.com>
2023-04-27 15:59:17 +0000

    feat(gas): Declare Kokoro artifacts

--- gas/.toys/gas/.test/test_kokoro_trigger.rb
+++ gas/.toys/gas/.test/test_kokoro_trigger.rb
@@ -31,6 +31,7 @@ describe "gas kokoro-trigger" do
 
   let(:exec_service) { Toys::Utils::Exec.new }
   let(:workspace_dir) { Dir.mktmpdir }
+  let(:artifacts_dir) { File.join workspace_dir, "artifacts" }
   let(:gem_platforms) do
     [
       "arm64-darwin",
@@ -52,7 +53,8 @@ describe "gas kokoro-trigger" do
       "GAS_PLATFORMS" => gem_platforms.join(":"),
       "GAS_RUBY_VERSIONS" => ruby_versions.join(":"),
       "GAS_RUBYGEMS_KEY_FILE" => "data2/keyfile.txt",
-      "GAS_WORKSPACE_DIR" => workspace_dir
+      "GAS_WORKSPACE_DIR" => workspace_dir,
+      "GAS_ARTIFACTS_DIR" => artifacts_dir
     }
   end
   let(:excluded_versions) do
@@ -61,6 +63,7 @@ describe "gas kokoro-trigger" do
       "x64-mingw-ucrt" => ["2.7", "3.0"]
     }
   end
+  let(:fake_gems) { ["fake-gem-1.0.gem", "fake-gem-2.0.gem"] }
 
   def temp_set_env hash
     old_env = hash.to_h do |key, val|
@@ -132,6 +135,25 @@ describe "gas kokoro-trigger" do
           assert File.exist? "#{gem_and_version}-#{platform}/lib/google/#{ruby}/protobuf_c.#{suffix}"
         end
       end
+    end
+
+    # Make sure we copied gems into the artifacts directory
+    gem_platforms.each do |platform|
+      artifact_name = "#{gem_and_version}-#{platform}.gem"
+      assert File.exist? "#{artifacts_dir}/#{artifact_name}"
+      artifact_content = File.read "#{artifacts_dir}/#{artifact_name}"
+      original_content = File.read "#{workspace_dir}/#{gem_and_version}/pkg/#{artifact_name}"
+      assert_equal original_content, artifact_content
+    end
+
+    # Make sure the artifacts directory includes the source and additional gems
+    original_content = File.read "#{__dir__}/data/#{gem_and_version}.gem"
+    artifact_content = File.read "#{artifacts_dir}/#{gem_and_version}.gem"
+    assert_equal original_content, artifact_content
+    fake_gems.each do |fake_gem|
+      original_content = File.read "#{__dir__}/data2/#{fake_gem}"
+      artifact_content = File.read "#{artifacts_dir}/#{fake_gem}"
+      assert_equal original_content, artifact_content
     end
   end
 end
