085dfda93c3ec879b99ef02ea963d452b7d55d61
Daniel Azuma <dazuma@google.com>
2024-06-11 11:09:44 -1000

    fix(owlbot): Fix multi-wrapper dependencies that were broken when dropping Ruby 2.7 (#328)

--- owlbot-postprocessor/lib/owlbot/multi_wrapper.rb
+++ owlbot-postprocessor/lib/owlbot/multi_wrapper.rb
@@ -227,7 +227,7 @@ module OwlBot
                        .select { |line| line.start_with? "  gem.add_dependency \"#{gem_name}" }
         end
         content = File.read "#{@final_gem}.gemspec"
-        content = content.sub(/(\n  gem\.add_dependency [^\n]+)\n\n/, "\\1\n#{lines.join}\n")
+        content = content.sub(/(\n  gem\.add_dependency [^\n]+)\nend/, "\\1\n#{lines.join}\n")
         File.write "#{@final_gem}.gemspec", content
       end
 
